f475a5dc2203a04661379c27e7800bc4e1b7caff
Süleyman Efe Metik <144699394+bensullu@users.noreply.github.com>
2025-02-17 15:23:16 +0300

    first commit

---
+++ hafta3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hafta3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Resule giriniz");
+            int sayi1=Convert.ToInt32(Console.ReadLine());
+            if (sayi1 > 0)
+            {
+                Console.WriteLine($"girdiğiniz {sayi1} sayısı pozitiftir");
+            }
+            else if (sayi1 <0)
+            {
+                Console.WriteLine($"girdiğiniz {sayi1} sayısı negatiftir");
+            }
+            else
+            { 
+                Console.WriteLine($"lütfen bir tam sayı giriniz");
+            }
+            
+
+        }
+    }
+}
